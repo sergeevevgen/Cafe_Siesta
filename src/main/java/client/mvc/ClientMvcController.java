3681dca1a5df059eda713d598beec9f85901e379
Иванов Иван <BROTON07@yandex.ru>
2023-04-13 02:37:27 +0400

    added getting data for profile.html

--- src/main/java/client/mvc/ClientMvcController.java
+++ src/main/java/client/mvc/ClientMvcController.java
@@ -5,6 +5,7 @@ import client.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,10 +27,10 @@ public class ClientMvcController {
         return "signup";
     }
 
-    @GetMapping("/profile")
-    public String getProfile(Model model) {
+    @GetMapping("/profile/{id}")
+    public String getProfile(@PathVariable Long id, Model model) {
         model.addAttribute("client",
-                userService.findById(14L));
+                userService.findById(id));
         return "profile";
     }
 }
