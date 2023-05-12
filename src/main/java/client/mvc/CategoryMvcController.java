3c8707faa20516f584ef29c9b8113cb9bdc2be76
Иванов Иван <BROTON07@yandex.ru>
2023-05-12 08:51:19 +0400

    add navigation for categories page

--- src/main/java/client/mvc/CategoryMvcController.java
+++ src/main/java/client/mvc/CategoryMvcController.java
@@ -1,18 +1,25 @@
 package client.mvc;
 
+import client.data.model.dto.ProductDto;
 import client.service.CategoryService;
+import client.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/category")
 public class CategoryMvcController {
     private final CategoryService categoryService;
+    private final ProductService productService;
 
-    public CategoryMvcController(CategoryService categoryService) {
+    public CategoryMvcController(CategoryService categoryService, ProductService productService) {
         this.categoryService = categoryService;
+        this.productService = productService;
     }
 
     @GetMapping
@@ -20,5 +27,12 @@ public class CategoryMvcController {
         model.addAttribute("categories",
                 categoryService.findAllCategories());
         return "categories-main";
+    }
+
+    @GetMapping("/products")
+    public String getProductsByCategory(@RequestParam(name = "category") String category, Model model) {
+        List<ProductDto> products = productService.findProductsByCategory(category);
+        model.addAttribute("products", products);
+        return "category";
     }
 }
