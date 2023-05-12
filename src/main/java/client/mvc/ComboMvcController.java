eb374f875e3fddadbe77aa7af4e1c5f2e29f4b7b
Иванов Иван <BROTON07@yandex.ru>
2023-05-12 12:44:56 +0400

    added combos pages

--- src/main/java/client/mvc/ComboMvcController.java
+++ src/main/java/client/mvc/ComboMvcController.java
@@ -2,14 +2,31 @@ package client.mvc;
 
 import client.service.ComboService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/combo")
+@RequestMapping("/combos")
 public class ComboMvcController {
     private final ComboService comboService;
 
     public ComboMvcController(ComboService comboService) {
         this.comboService = comboService;
+    }
+
+    @GetMapping
+    public String GetAllCombos(Model model) {
+        model.addAttribute("combos",
+                comboService.findAllCombos());
+        return "combos";
+    }
+
+    @GetMapping("/{id}")
+    public String getCombo(@PathVariable Long id, Model model) {
+        model.addAttribute("combo",
+                comboService.findCombo(id));
+        return "combo";
     }
 }
