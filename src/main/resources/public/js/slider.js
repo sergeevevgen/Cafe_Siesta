2e85b9505ebe548b869b8721dbdc1743136a6317
superhugebetformiladze <broton07@yandex.ru>
2023-05-12 19:35:23 +0400

    add combo pages for admin

---
+++ src/main/resources/public/js/slider.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
