9ff72f64cb4b4c52a74a3b1298ef269fb0e844f4
Иванов Иван <BROTON07@yandex.ru>
2023-05-15 01:40:54 +0400

    fix counter

--- src/main/resources/public/js/counter.js
+++ src/main/resources/public/js/counter.js
@@ -1,21 +1,23 @@
-const countText = document.querySelector("#count"); // выбираем элемент, в котором хранится количество товаров
-const plusButton = document.querySelector("#plus"); // выбираем кнопку увеличения количества товаров
-const minusButton = document.querySelector("#minus"); // выбираем кнопку уменьшения количества товаров
+window.addEventListener("DOMContentLoaded", (event) => {
+    const countText = document.getElementById("count"); // выбираем элемент, в котором хранится количество товаров
+    const plusButton = document.getElementById("plus"); // выбираем кнопку увеличения количества товаров
+    const minusButton = document.getElementById("minus"); // выбираем кнопку уменьшения количества товаров
+    let count = 0;
+    console.log(minusButton);
+    console.log(plusButton);
+    plusButton.addEventListener("click", function () { // добавляем обработчик события на клик по кнопке увеличения количества товаров
+        console.log("Клик на кнопке 'минус'");
+        if (count < 20) {
+            count++; // увеличиваем количество товаров на 1
+            countText.innerText = count; // обновляем количество товаров на странице
+        }
+    });
 
-let count = 0;
-console.log(minusButton);
-plusButton.addEventListener("click", function () { // добавляем обработчик события на клик по кнопке увеличения количества товаров
-    console.log("Клик на кнопке 'минус'");
-    if (count < 20) {
-        count++; // увеличиваем количество товаров на 1
-        countText.innerText = count; // обновляем количество товаров на странице
-    }
-});
-
-minusButton.addEventListener("click", function () { // добавляем обработчик события на клик по кнопке уменьшения количества товаров
-    console.log("Клик на кнопке 'Плюс'");
-    if (count > 0) { // проверяем, что количество товаров больше 0, чтобы не получить отрицательное количество
-        count--; // уменьшаем количество товаров на 1
-        countText.innerText = count; // обновляем количество товаров на странице
-    }
+    minusButton.addEventListener("click", function () { // добавляем обработчик события на клик по кнопке уменьшения количества товаров
+        console.log("Клик на кнопке 'плюс'");
+        if (count > 0) { // проверяем, что количество товаров больше 0, чтобы не получить отрицательное количество
+            count--; // уменьшаем количество товаров на 1
+            countText.innerText = count; // обновляем количество товаров на странице
+        }
+    });
 });
