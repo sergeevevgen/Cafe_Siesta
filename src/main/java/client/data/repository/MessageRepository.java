e56c36bcd86cc1698d538af02426697f11684f9d
Евгений Сергеев <alexevgenii@mail.ru>
2023-05-17 17:56:23 +0400

    DOne antonov controllers

--- src/main/java/client/data/repository/MessageRepository.java
+++ src/main/java/client/data/repository/MessageRepository.java
@@ -2,9 +2,15 @@ package client.data.repository;
 
 import client.data.model.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     void deleteAllByChatId(Long chat_id);
+
+    @Query(value = "select * from message m where m.chat_fk = :chat_id", nativeQuery = true)
+    List<Message> findAllByChat(Long chat_id);
 }
