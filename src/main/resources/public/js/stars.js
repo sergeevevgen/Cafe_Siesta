8c12be26242458e435da140104fa64ad9ba73a1a
Иванов Иван <BROTON07@yandex.ru>
2023-05-20 02:27:04 +0400

    stars

--- src/main/resources/public/js/stars.js
+++ src/main/resources/public/js/stars.js
@@ -1,11 +1,17 @@
-// Get all the stars
-const stars = document.querySelectorAll(".star");
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Get all the stars
+    const stars = document.querySelectorAll(".star");
 
-// Add click event listener to each star
-stars.forEach((star) => {
-  star.addEventListener("click", () => {
-    stars.forEach((s) => s.classList.remove("active"));
-    star.classList.add("active");
-  });
+    // Add click event listener to each star
+    stars.forEach((star) => {
+        star.addEventListener("click", () => {
+            stars.forEach((s) => s.classList.remove("active"));
+            star.classList.add("active");
+
+            const ratingInput = document.querySelector('input[name="rating-value"]');
+            ratingInput.value = star.dataset.value;
+            console.log("звезды", star.dataset.value);
+            console.log(ratingInput.value);
+        });
+    });
 });
-
